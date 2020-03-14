13baa17bd19bfd6e35f8fa01bd2585386e5aa7eb
hiro <hiro.chibita@gmail.com>
2020-03-14 14:09:25 +0000

    ログアウト機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,10 @@ protect_from_forgery with: :exception
 add_flash_types :success, :info, :warning, :danger
 
 helper_method :current_user, :logged_in?
+  def log_in(user)
+   session[:user_id] = user.id
+  end 
+  
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
   end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,14 +14,21 @@ class SessionsController < ApplicationController
     end  
   end
   
+  def destroy
+    log_out
+    redirect_to root_url, info: 'ログアウトしました'
+  end 
+  
   private
    def log_in(user)
      session[:user_id] = user.id
-   end  
+   end 
+   
    def log_out
      session.delete(:users_id)
      @current_user = nil
    end
+
    def user_params
     params.require(:user).permit(:email,:password)
    end
